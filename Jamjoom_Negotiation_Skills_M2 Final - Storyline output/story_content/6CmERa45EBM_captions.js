a7e121bc17159ee624fea02bec85266ec27a8b39
ARDLMS <118976020+ARDLMS@users.noreply.github.com>
2023-05-14 18:32:46 +0300

    2

---
+++ Jamjoom_Negotiation_Skills_M2 Final - Storyline output/story_content/6CmERa45EBM_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.69.28997.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:01.676%0D%0A%D9%85%D8%AB%D9%84%D8%AB%20%D8%A7%D9%84%D9%85%D9%81%D8%A7%D9%88%D8%B6%D8%A7%D8%AA%0D%0A%0D%0A"}')
